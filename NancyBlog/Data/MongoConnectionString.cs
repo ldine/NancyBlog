1890772f91a0137468dda56f641272b2e49e68c3
Lawrence Dine <lawrencedine@googlemail.com>
2012-03-19 23:08:48 +0000

    Added Simple.Data and a connectionstring to a mongo database

---
+++ NancyBlog/Data/MongoConnectionString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace NancyBlog.Data
+{
+    public static class MongoConnectionString
+    {
+        public static string Retrieve()
+        {
+            return ConfigurationManager.ConnectionStrings[Environment.MachineName].ConnectionString;
+        }
+    }
+}
---
+++ NancyBlog/Data/MongoDatabaseProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Simple.Data;
+
+namespace NancyBlog.Data
+{
+    public class MongoDatabaseProvider : IDatabaseProvider
+    {
+
+        public dynamic GetDb()
+        {
+            var connectionString = MongoConnectionString.Retrieve();
+            return Database.OpenConnection(connectionString);
+        }
+    }
+
+    public interface IDatabaseProvider
+    {
+        dynamic GetDb();
+    }
+}
