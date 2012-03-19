b75638427a9c4229723080375b2cf02945163561
Lawrence Dine <lawrencedine@googlemail.com>
2012-03-19 23:13:35 +0000

    Added models for User and Post

---
+++ NancyBlog/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace NancyBlog.Models
+{
+    public class Post
+    {
+        public virtual ObjectId Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Body { get; set; }
+        public virtual ObjectId AuthorId { get; set; }
+        public virtual string Author { get; set; }
+    }
+}
---
+++ NancyBlog/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace NancyBlog.Models
+{
+    public class User
+    {
+        public virtual ObjectId Id { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+        public virtual DateTime Joined { get; set; }
+    }
+}
